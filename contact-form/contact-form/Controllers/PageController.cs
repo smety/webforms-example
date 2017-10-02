b85828c2013cb4332e4d7c150d10eb496724fe08
Tomas Smetka <tomas.smetka@rohlik.cz>
2017-10-02 22:27:46 +0200

    Add variable to render

--- contact-form/contact-form/Controllers/PageController.cs
+++ contact-form/contact-form/Controllers/PageController.cs
@@ -4,18 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace contactform.Controllers
 {
+
+
     public class PageController : Controller
     {
+
         public ActionResult Index()
         {
-            return View ();
+			ViewBag.Name = "Monjurul Habib";
+
+            return View();
         }
 
-		public ActionResult About()
-		{
-			return View ();
-		}
+        public ActionResult About()
+        {
+	
+            //	ViewBag.Age = 56;
+
+        //    ViewBag.MyMessageToUsers = "Hello from me.";
+          //  ViewBag.AnswerText = "Your answer goes here.";
+
+            return View();
+        }
     }
 }
