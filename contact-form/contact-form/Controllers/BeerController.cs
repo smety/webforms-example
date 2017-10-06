353fafb298e358a903a461af9b91c60e3a4602a4
Tomas Smetka <tomas.smetka@rohlik.cz>
2017-10-06 11:29:16 +0200

    Add API 'Beer' controller

---
+++ contact-form/contact-form/Controllers/BeerController.cs
@@ -0,0 +1,45 @@
+﻿namespace WebForms
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Net;
+	using System.Net.Http;
+	using System.Web.Http;
+
+	public class BeerController : ApiController
+	{
+        // Mock data
+        Person[] Personals = new Person[]
+		{
+            new Person { Id = 1, Name = "Tomas Smetka", Beer = 2, Sex = "male"},
+			new Person { Id = 2, Name = "Homer Simpson", Beer = 3, Sex = "female"},
+            new Person { Id = 3, Name = "Barney Gumble", Beer = 5, Sex = "male"},
+			new Person { Id = 10, Name = "Vocko", Beer = 10, Sex = "male"}
+		};
+
+        // Interface to @Person array
+        public IEnumerable<Person> GetAllPersonals()
+		{
+            return Personals;
+		}
+
+        // Get all personals by ID
+		public Person GetPersonalsById(int id)
+		{
+            var person = Personals.FirstOrDefault((p) => p.Id == id);
+			if (person == null) // check exist 
+			{
+				throw new HttpResponseException(HttpStatusCode.NotFound); // not found
+			}
+			return person; // return personals order by ID
+		}
+
+        public IEnumerable<Person> GetPersonalByBeer(string personals)
+		{
+			return Personals.Where(  
+                (p) => string.Equals(p.Name, personals,
+					StringComparison.OrdinalIgnoreCase));
+		}
+	}
+}
---
+++ contact-form/contact-form/Person.cs
@@ -0,0 +1,7 @@
+﻿public class Person
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public decimal Beer { get; set; }
+	public string Sex { get; set; }
+}
